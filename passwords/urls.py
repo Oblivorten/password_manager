901e73cc4a5ad18987d5514e39d42620c75eb79b
Nikita <oblivorten@gmail.com>
2025-03-24 22:10:23 +0300

    Создал шаблоны и добавил авторизацию

--- passwords/urls.py
+++ passwords/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('passwords/', views.password_list, name='password_list'),
     path('passwords/add/', views.password_add, name='password_add'),
     path('passwords/generate/', views.generate_random_password_view, name='generate_random_password'),
+    path('login/', views.login_view, name='login'),
 ]
