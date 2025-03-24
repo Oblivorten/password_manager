4e91ae568921f72be51d99afec895ad93a964de3
Nikita <oblivorten@gmail.com>
2025-03-24 21:55:21 +0300

    Добавлена функция сохранения и генерации случайного пароля

--- passwords/forms.py
+++ passwords/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Password
+
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +10,16 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class PasswordForm(forms.ModelForm):
+    class Meta:
+        model = Password
+        fields = ['site', 'username', 'password']
+
+    def save(self, commit=True):
+        password = super().save(commit=False)
+        password.user = self.instance.user  # привязываем к текущему пользователю
+        if commit:
+            password.save()
+        return password
--- passwords/urls.py
+++ passwords/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('passwords/', views.password_list, name='password_list'),
+    path('passwords/add/', views.password_add, name='password_add'),
+    path('passwords/generate/', views.generate_random_password_view, name='generate_random_password'),
 ]
